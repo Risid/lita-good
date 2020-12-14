4b906e4860bfe7243023c0ae93d6bba5b88b4ce1
Xin Wang <hsimwong1202@gmail.com>
2020-12-14 16:06:29 +0800

    solved probem occured when source of translation is non-ascii

--- lita-translator/lib/lita/handlers/translator.rb
+++ lita-translator/lib/lita/handlers/translator.rb
@@ -12,14 +12,15 @@ module Lita
       )
 
       def respond_with_translation(response)
-        n = response.match_data.captures.first
-        response.reply("Lita正在#{n}的英文...")
+        n = response.match_data.captures.first.encode('utf-8')
+        response.reply("Lita正在#{n}的中文...")
         salt = "1435660288"
         appid = "20201214000647087"
         secCode = "P4Xaa4qbJkFQ6L3Bmrk_"
         sign = Digest::MD5.hexdigest("#{appid}#{n}#{salt}#{secCode}")
         query = "http://api.fanyi.baidu.com/api/trans/vip/translate?q=#{n}&from=auto&to=zh&appid=#{appid}&salt=#{salt}&sign=#{sign}"
-        http_response = http.get(query)
+        uri = URI.parse(URI.escape(query))
+        http_response = http.get(uri)
         # print(query)
         data = MultiJson.load(http_response.body)
         response.reply("#{n}的中文翻译是#{data["trans_result"][0]["dst"]}")
